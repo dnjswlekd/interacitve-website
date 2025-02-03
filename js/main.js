84206383efc74618a2c2c8a7179962bf5de8f664
dnjswlekd <wj8014@gmail.com>
2025-02-03 22:30:43 +0900

    [Fix] blend image top fixed

--- js/main.js
+++ js/main.js
@@ -422,12 +422,73 @@
           )})`;
         }
 
+        // current3에서 쓰는 캔버스를 미리 그려주기 시작
+
+        if (scrollRatio > 0.9) {
+          const widthRatio = document.body.offsetWidth / objs.canvas.width;
+          const heightRatio = window.innerHeight / objs.canvas.height;
+          let canvasScaleRatio;
+
+          if (widthRatio <= heightRatio) {
+            // 캔버스보다 브라우저 창이 홀쭉한 경우
+            canvasScaleRatio = heightRatio;
+          } else {
+            // 캔버스보다 브라우저 창이 납작한 경우
+            canvasScaleRatio = widthRatio;
+          }
+
+          objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+          objs.context.fillStyle = 'white';
+          objs.context.drawImage(objs.images[0], 0, 0);
+
+          // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
+          const recalculatedInnerWidth =
+            document.body.offsetWidth / canvasScaleRatio;
+          const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+          const whiteRectWidth = recalculatedInnerWidth * 0.15;
+          values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
+          values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
+          values.rect2X[0] =
+            values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
+          values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
+
+          //좌우 흰색 박스 그리기
+          // objs.context.fillRect(
+          //   values.rect1X[0],
+          //   0,
+          //   parseInt(whiteRectWidth),
+          //   objs.canvas.height
+          // ),
+          //   objs.context.fillRect(
+          //     values.rect2X[0],
+          //     0,
+          //     parseInt(whiteRectWidth),
+          //     recalculatedInnerHeight
+          //   );
+
+          objs.context.fillRect(
+            parseInt(values.rect1X[0]),
+            0,
+            parseInt(whiteRectWidth),
+            objs.canvas.height
+          );
+          objs.context.fillRect(
+            parseInt(values.rect2X[0]),
+            0,
+            parseInt(whiteRectWidth),
+            objs.canvas.height
+          );
+        }
+
         break;
 
       case 3:
         // console.log('3 play');
+        let step = 0;
+
         // 가로/세로 모두 꽉 차게 하기 위해 여기서 세팅 (계산 필요함)
-        const widthRatio = document.body.offsetWidth / objs.canvas.width;
+        const widthRatio = window.innerWidth / objs.canvas.width;
         const heightRatio = window.innerHeight / objs.canvas.height;
         let canvasScaleRatio;
 
@@ -492,6 +553,22 @@
           parseInt(whiteRectWidth),
           objs.canvas.height
         );
+
+        if (scrollRatio < values.rect1X[2].end) {
+          // 캔버스가 브라우저 상단에 닿기 전
+          step = 1;
+          objs.canvas.classList.remove('sticky');
+          console.log('캔버스가 브라우저 상단에 닿기 전');
+        } else {
+          // 블렌드
+          step = 2;
+          objs.canvas.classList.add('sticky');
+          objs.canvas.style.top = `${
+            -(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2
+          }px`;
+          console.log('캔버스가 브라우저 상단에 닿은 후');
+        }
+
         break;
     }
   }
